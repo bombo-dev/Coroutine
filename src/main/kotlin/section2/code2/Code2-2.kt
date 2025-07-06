aeebe4cc22f9da8148f078b17c8f2c9f6d8f22b9
TI-Bombo <bombo96@chungwoon.ac.kr>
2025-07-06 15:51:00 +0900

    coroutine 단위의 Naming 적용

--- src/main/kotlin/section2/code2/Code2-2.kt
+++ src/main/kotlin/section2/code2/Code2-2.kt
@@ -1,12 +1,15 @@
 package section2.code2
 
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking<Unit> {
+// Debugging VM arguments -Dkotlinx.coroutines.debug
+// CoroutineName은 Coroutine Context의 하위 인터페이스 구현 객체로 CoroutineContext.Element에 해당한다.
+fun main() = runBlocking<Unit>(CoroutineName("runBlocking 코루틴")) {
     println("[${Thread.currentThread().name}] Hello, Coroutines[runBlocking]!")
 
-    launch {
+    launch(CoroutineName("Launch 코루틴")) {
         println("[${Thread.currentThread().name}] Hello, Coroutines[Launch]!")
     }
     // 아래 코드에 대한 사용이 가능하나, runBlocking 안에서 다시 runBlocking을 사용하면 상위에 있는 가상 스레드에 대해서 블로킹을 한다.
