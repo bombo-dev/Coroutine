783a5eb7d82767ad68296affc6752d7621a0fbd9
TI-Bombo <bombo96@chungwoon.ac.kr>
2025-07-06 20:58:38 +0900

    Coroutine 취소 후 동작 중 순차성 이슈

---
+++ src/main/kotlin/section4/Code4-10.kt
@@ -0,0 +1,16 @@
+package section4
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.lang.Thread.sleep
+
+fun main() = runBlocking<Unit> {
+    val longJob = launch(Dispatchers.Default) {
+        sleep(1000L)
+        println("longJob 코루틴 로직")
+    }
+    longJob.cancel() // 코루틴 취소
+    println("코루틴 취소 후 실행되어야 하는 로직")
+}
