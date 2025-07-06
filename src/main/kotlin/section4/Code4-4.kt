03c7e6657783d6a59443f19178777150f6f44f48
TI-Bombo <bombo96@chungwoon.ac.kr>
2025-07-06 17:24:07 +0900

    한 번에 join거는 순차처리
    - 역시 all이 있을거라 생각했어

--- src/main/kotlin/section4/Code4-4.kt
+++ src/main/kotlin/section4/Code4-4.kt
@@ -1,6 +1,7 @@
 package section4
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.lang.Thread.sleep
@@ -19,8 +20,9 @@ fun main() = runBlocking<Unit> {
     // join을 하면 Job이 완료될 때 까지 상위 Thread를 일시정지 시킨다. yield 한다.
     // yield의 위치를 적절히 선언해주어야, 순차 처리 시 지연 문제가 발생하지 않을 수 있으니 주의하기.
     // 만약, convertedImage1 아래로 가면, 1초 기다리고, 다시 2를 위해서 1초 기다리고 완료된다.
-    convertedImage1.join()
-    convertedImage2.join()
+//    convertedImage1.join()
+//    convertedImage2.join()
+    joinAll(convertedImage1, convertedImage2)
 
     launch(Dispatchers.Default) {
         println("[${Thread.currentThread().name}] 이미지 업로드")
