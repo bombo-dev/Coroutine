76ff3a460ccaa5678125d22af67187d7f1abf272
TI-Bombo <bombo96@chungwoon.ac.kr>
2025-07-20 17:02:55 +0900

    Coroutine에서 동시성을 예방하기 위한 Lock 함수 - ReentrantLock 도 사용가능하나, 스레드를 블로킹함.

--- src/main/kotlin/section11/Code11-1.kt
+++ src/main/kotlin/section11/Code11-1.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.sync.withLock
 
 fun main() {
     var count = 0
-    val mutex = Mutex()
+    val mutex = Mutex() // ReentrantLock은 쓰레드를 블로킹하나 Coroutine의 Mutex는 일시중단하여 스레드를 양보함.
     runBlocking {
         repeat(10000) {
             mutex.withLock {
